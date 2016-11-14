593b7fe0f20b38327ac672c6e2fc2ac3e3f70ef9
davidhanimann <davidhanimann@gmail.com>
2016-11-14 14:49:13 +0100

    Load GeoJSON

--- Master.R
+++ Master.R
@@ -1,1 +1,9 @@
-# New file
+# Set default directory
+setwd("~/Desktop/SpatAnalysisI/Networking/Networking/")
+
+# Import GeoJSON
+# Read data from JSON and cconvert to Spatial data frame
+velo <- fromJSON("data/veloweg.json", flatten = FALSE)
+coords <- matrix(unlist(velo$features$geometry$coordinates), ncol = 2, byrow = TRUE)
+velo <- SpatialLinesDataFrame(coords = coords, data = velo$features$properties, proj4string = CRS("+init=epsg:4326"))
+
